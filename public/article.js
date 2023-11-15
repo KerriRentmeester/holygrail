83a70d93e3b9c4046373ae2fe8bf7273dd3fcf8a
KerriRentmeester <130302053+KerriRentmeester@users.noreply.github.com>
2023-11-15 09:47:04 -0600

    more tiny tweaks

--- public/article.js
+++ public/article.js
@@ -2,7 +2,7 @@ function Article(props){
     return (<>
         <article>
             <PlusMinus section="article" handle={props.handle}/>  {/* pass a props of handle onto the article comp */}
-            <div className="section">Name of section: Article:{props.data.article}</div>
+            <div className="section"> Article:{props.data.article}</div>
             <Data data={props.data}/>  {/* data is passed in thru props */}
         </article>
     </>);
